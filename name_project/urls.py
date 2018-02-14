32323c295fbe8dc62bdb6a982856ec9d9e22d4d8
Hackerman55 <rustamomsk57@mail.ru>
2018-02-14 17:17:27 +0600

    3 practice

--- name_project/urls.py
+++ name_project/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:article_id>/', views.get_article, name = 'article'),
+    path('new/', views.new_article, name='new_article')
 ]
--- name_project/views.py
+++ name_project/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Article, Author
+from .forms import ArticleForm
 # Create your views here.
 def index(request):
+    articles = Article.objects.all()
     return render(request, 'name_project/news_list.html', {'articles': articles})
 
 def get_article(request, article_id):
-    return render(request, 'name_project/news_article.html', {'articles': articles[article_id - 1]})
+    try:
+        articles = Article.objects.get(id = article_id)
+        author = Author.objects.filter(article__pk=article_id)
+    except (Article.DoesNotExist, Author.DoesNotExist) as error:
+        return HttpResponseNotFound(error)
+    return render(request, 'name_project/news_article.html',
+                  {'articles': articles,
+                   'author' : author
+                   }
+                  )
+def new_article(request):
+    form = ArticleForm()
+    return render(request, 'name_project/edit_article.html', {'form': form})
 
-articles = [
+
+
+'''articles = [
    {
        'id': 1,
        'title': 'First news',
@@ -18,4 +35,4 @@ articles = [
        'id': 2,
        'title': 'Second news',
        'text': 'This is the amazing second article'
-   }]
+   }]'''
